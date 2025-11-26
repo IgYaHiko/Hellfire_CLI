37d74e4a432f5318b256e2f97d0d86c0ff3b0572
IgYaHiko <subhrokolay2@gmail.com>
2025-11-26 23:31:25 +0530

    [main]: fixed-auth-client-client-side

--- client/lib/auth-client.ts
+++ client/lib/auth-client.ts
@@ -1,5 +1,6 @@
 import {createAuthClient} from 'better-auth/react'
-import { deviceAuthorizationClient } from "better-auth/plugins"
+import { deviceAuthorizationClient } from "better-auth/client/plugins"
+
 export const authClient = createAuthClient({
     baseURL: "http://localhost:3005",
       plugins: [ 
